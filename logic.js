edc3baff4785971f8b233b6e0bd6249c0e74e17f
Navjot Singh Virk <virksaabnavjot@gmail.com>
2019-12-23 00:45:12 +0000

    New data exposed, Bug fixes and Basic UI relaease

--- logic.js
+++ logic.js
@@ -17,57 +17,91 @@ fetch(url)
 
     //individual ticker - need to loop through 500 times at least
     var i;
-    for (i = 0; i < tickers.rows.length; i++) {
+    for (i = 1; i < tickers.rows.length; i++) {
       var ticker = tickers.rows.item(i);
 
       parseTickerInformation(ticker);
     }
     quick(sp500_JSON);
-    createCompaniesList();
+    //quick(JSON.stringify(sp500_JSON));
+    createTableFromJSON(sp500_JSON);
 
   });
 
-
-
 //get the core info about the company
 function parseTickerInformation(ticker) {
-  var tickerSymbol = ticker.cells[0].innerText;
-  var exchangeString = ticker.cells[0].innerHTML;
-  var exchange = (exchangeString.includes("nasdaq")) ? "NASDAQ" : "NYSE";
-  var companyName = ticker.cells[1].innerText;
+  var symbol = ticker.cells[0].innerText.trim();
+  var name = ticker.cells[1].innerText;
+  var exchange = ((ticker.cells[0].innerHTML).includes("nasdaq")) ? "NASDAQ" : "NYSE";
+  let industry = ticker.cells[3].innerText;
+  let sector = ticker.cells[4].innerText;
+  let founded = ticker.cells[8].innerText.trim();
+  let headquarters = ticker.cells[5].innerText;
+  let date_first_added = ticker.cells[6].innerText;
+  let cik = ticker.cells[7].innerText;
+  let sec_filings = "https://www.sec.gov/cgi-bin/browse-edgar?CIK=" + symbol + "&action=getcompany";
+  let link = (exchange.includes("NASDAQ")) ? "https://www.nasdaq.com/market-activity/stocks/" + symbol : "https://www.nyse.com/quote/XNYS:" + symbol;
+  let yahoo_finance = "https://finance.yahoo.com/quote/" + symbol;
+
   var company = {
-    ticker: tickerSymbol,
-    exchange: exchange,
-    name: companyName
+    "symbol": symbol,
+    "name": name,
+    "exchange": exchange,
+    "industry": industry,
+    "sector": sector,
+    "founded": founded,
+    "headquarters": headquarters,
+    "date_first_added": date_first_added,
+    "cik": cik,
+    "sec_filings": sec_filings,
+    "link": link,
+    "yahoo_finance": yahoo_finance
   };
 
   sp500_JSON.push(company);
-
 }
 
+function createTableFromJSON(data) {
+  var sp500_JSON = data;
+  var col = [];
+  for (var i = 0; i < sp500_JSON.length; i++) {
+    for (var key in sp500_JSON[i]) {
+      if (col.indexOf(key) === -1) {
+        col.push(key);
+      }
+    }
+  }
 
+  var table = document.createElement("table");
+  var tr = table.insertRow(-1);
 
-function createCompaniesList() {
+  for (var i = 0; i < col.length; i++) {
+    var th = document.createElement("th");
+    th.innerHTML = col[i].toUpperCase();
+    tr.appendChild(th);
+  }
 
-  let table = document.getElementById("list");
-  let row = document.createElement("tr");
-  createTableData();
+  for (var i = 0; i < sp500_JSON.length; i++) {
+    tr = table.insertRow(-1);
 
+    for (var j = 0; j < col.length; j++) {
+      var tabCell = tr.insertCell(-1);
+      tabCell.innerHTML = sp500_JSON[i][col[j]];
+    }
+  }
 
-
-
+  var div = document.getElementById("companieslist");
+  div.innerHTML = "";
+  div.appendChild(table);
 }
 
-function createTableData() {
-
-}
 //quick console
 function quick(input) {
   console.log(input);
 }
 
-
-
 /*
-Help doc: https://stackoverflow.com/questions/53127383/how-to-pull-data-from-wikipedia-page
+Help docs:
+https://stackoverflow.com/questions/53127383/how-to-pull-data-from-wikipedia-page
+https://www.encodedna.com/javascript/populate-json-data-to-html-table-using-javascript.htm
 */
