14ad70b62863694f514a2ceb87b62c2ce16504a9
Navjot Singh Virk <virksaabnavjot@gmail.com>
2025-02-10 00:21:39 +0530

    MRVIRKCOM-98 [Code Fixes] For Data source changes  + Better Labelling
    Wiki had changed the data source where the data array reduced from size of 8 to 7 leading to the tool breaking this need us to adjust the code to cater for the source data changes
    
    Also did User experience improvement  with better labelling of information

--- logic.js
+++ logic.js
@@ -33,24 +33,24 @@ function parseTickerInformation(ticker) {
   let symbol = ticker.cells[0].innerText.trim();
   let exchange = ((ticker.cells[0].innerHTML).includes("nasdaq")) ? "NASDAQ" : "NYSE";
   let industry = ticker.cells[3].innerText;
-  let sector = ticker.cells[4].innerText;
-  let founded = ticker.cells[8].innerText.trim();
-  let headquarters = ticker.cells[5].innerText;
-  let date_first_added = ticker.cells[6].innerText;
-  let cik = ticker.cells[7].innerText;
+  let sector = ticker.cells[2].innerText;
+  let founded = ticker.cells[7].innerText.trim();
+  let headquarters = ticker.cells[4].innerText;
+  let date_first_added = ticker.cells[5].innerText;
+  let cik = ticker.cells[6].innerText;
   let sec_filings = "https://www.sec.gov/cgi-bin/browse-edgar?CIK=" + symbol + "&action=getcompany";
   let exchange_profile = (exchange.includes("NASDAQ")) ? "https://www.nasdaq.com/market-activity/stocks/" + symbol : "https://www.nyse.com/quote/XNYS:" + symbol;
   let yahoo_finance_profile = "https://finance.yahoo.com/quote/" + symbol;
   let dividends = "https://seekingalpha.com/symbol/" + symbol + "/dividends";
 
   let company = {
-    "name": name,
-    "symbol": symbol,
-    "exchange": exchange,
-    "industry": industry,
-    "sector": sector,
-    "founded": founded,
-    "headquarters": headquarters,
+    "Name": name,
+    "Symbol": symbol,
+    "Exchange": exchange,
+    "Industry": industry,
+    "Sector": sector,
+    "Founded": founded,
+    "Headquarters": headquarters,
     "date_first_added": date_first_added,
     "cik": cik,
     "sec_filings": sec_filings,
@@ -72,8 +72,8 @@ function createList(data) {
     for (var key in data[i]) {
       let div = document.createElement('div');
 
-      if (key == "name") {
-        div.innerHTML = "<a href =" + data[i].yahoo_finance_profile + " class='companyLinks' target='_blank'>" + data[i][key] + "</a>";
+      if (key == "Name") {
+        div.innerHTML = "Company:"+ "<a href =" + data[i].yahoo_finance_profile + " class='companyLinks' target='_blank'>" + data[i][key] + "</a>";
       } else if (key == "sec_filings") {
         div.innerHTML = "<a href =" + data[i][key] + " class='companyLinks' target='_blank' rel='nofollow'>SEC Filings</a>";
       } else if (key == "exchange_profile") {
@@ -82,8 +82,10 @@ function createList(data) {
         div.innerHTML = "<a href =" + data[i][key] + " class='companyLinks' target='_blank' rel='nofollow'>Yahoo Finance Profile</a>";
       } else if (key == "dividends") {
         div.innerHTML = "<a href =" + data[i][key] + " class='companyLinks' target='_blank' rel='nofollow'>Dividend Information</a>";
+      } else if (key == "date_first_added"){
+        div.innerHTML = "Included on: "+data[i][key];
       } else {
-        div.innerHTML = data[i][key];
+        div.innerHTML = key+": "+data[i][key];
       }
 
       contentDiv.appendChild(div);
