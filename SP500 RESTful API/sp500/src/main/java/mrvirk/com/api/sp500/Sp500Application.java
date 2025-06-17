619072719e8a56650a6737046a70385877e02671
Navjot Singh Virk <virksaabnavjot@gmail.com>
2025-06-17 21:15:32 +0100

    MRVIRKCOM-118 - Outline code for Java Spring RESTful API -
    Its time to convert the application backend into an REST API to be make available more widely, currently its a simple implementation in javascript

---
+++ SP500 RESTful API/sp500/src/main/java/mrvirk/com/api/sp500/Sp500Application.java
@@ -0,0 +1,13 @@
+package mrvirk.com.api.sp500;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp500Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp500Application.class, args);
+	}
+
+}
---
+++ SP500 RESTful API/sp500/src/test/java/mrvirk/com/api/sp500/Sp500ApplicationTests.java
@@ -0,0 +1,13 @@
+package mrvirk.com.api.sp500;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp500ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
